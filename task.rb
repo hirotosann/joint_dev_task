87500e96b03ec51e01f59c979ccfe01c57543e24
hiroto <hiroto_apple@icloud.com>
2021-03-15 08:51:53 +0900

    q1~q2を回答

--- task.rb
+++ task.rb
@@ -5,7 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names << "斎藤"
+  p names
 end
 
 def q2
@@ -13,7 +14,9 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array1.push(array2)
+  array1.flatten!
+  p array1
 end
 
 def q3
